b30cf6468b42e9e9b169e041623ec6a4bbf2e0de
Nahuel Flores <nahuelhernanflores25@gmail.com>
2024-10-14 15:19:11 -0300

    CHORE: add path

--- src/main/java/server/soap/RouterSoap.java
+++ src/main/java/server/soap/RouterSoap.java
@@ -2,7 +2,11 @@ package server.soap;
 
 public class RouterSoap {
 	
+	// COUNTRY
 	public final static String NAMESPACE_COUNTRY = "http://soap/countries";
+	public final static String RESOURCE_COUNTRY  = "xsd/countries.xsd";
+	
+	// GENERAL
 	public final static String URLMAPPINGS  = "/ws/*";
 
 }
